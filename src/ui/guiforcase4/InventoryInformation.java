41b60705635ba7370f2f2f78bb6b971b979b2a04
ekiehuang <ekie729@gmail.com>
2020-04-19 18:34:35 -0700

    adjust the JTable(adding sorting, back button, color, size), handle no selection exception

--- src/ui/guiforcase4/InventoryInformation.java
+++ src/ui/guiforcase4/InventoryInformation.java
@@ -10,6 +10,7 @@ import service.VehicleSearchFilterElement;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -25,7 +26,7 @@ public class InventoryInformation extends JFrame {
   //Use JTable to show the related info of that vehicle
   private JTable table;
   private VehicleManagerImpl vmi;
-  DefaultTableModel VModel = new DefaultTableModel();
+  DefaultTableModel vModel = new DefaultTableModel();
 
 
   public InventoryInformation(int dID) {
@@ -37,23 +38,25 @@ public class InventoryInformation extends JFrame {
 
   private void create() {
     VehicleManagerImpl vmi = new VehicleManagerImpl();
-    table = new JTable(VModel);
-    String header[]=new String[]{"VehicleID","Make","Model","Price","Mileage"};
-    VModel.setColumnIdentifiers(header);
-    table.setModel(VModel);
+    table = new JTable(vModel);
+    String header[] = new String[]{"VehicleID", "VIN", "Make", "Model", "Price", "Mileage"};
+    vModel.setColumnIdentifiers(header);
+    table.setModel(vModel);
+    table.setAutoCreateRowSorter(true);
+    table.getTableHeader().setFont(new Font("Arial", PLAIN, 15));
     Collection<Vehicle> veh = vmi.getVehiclesBasedOnDealerId(dID);
     for (Vehicle v : veh) {
-      VModel.addRow(new Object[]{v.getVehicleId(),v.getMake(),v.getModel(),v.getPrice(),v.getMileage()});
+      vModel.addRow(new Object[]{v.getVehicleId(), v.getVin(), v.getMake(), v.getModel(), v.getPrice(), v.getMileage()});
     }
-
   }
 
   private void initialFrame() {
 
     JFrame frame = new JFrame("Inventory of Dealer" + this.dID);
-    frame.setSize(520, 500);
+    frame.setSize(570, 520);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     JPanel panel = new JPanel(null);
+    panel.setBackground(new Color(0, 0, 0, 0));
     frame.add(panel);
     addComponents(frame, panel);
     frame.setVisible(true);
@@ -62,12 +65,12 @@ public class InventoryInformation extends JFrame {
   private void addComponents(JFrame frame, JPanel panel) {
     JLabel jl = new JLabel("Inventory of Dealer" + this.dID);
     jl.setFont(new Font("Arial", PLAIN, 20));
-    jl.setForeground(Color.BLUE);
+    jl.setForeground(Color.BLACK);
     jl.setHorizontalAlignment(JTextField.CENTER);
-    jl.setBounds(120, 15, 280, 30);
+    jl.setBounds(140, 15, 280, 30);
     panel.add(jl);
     //(Ekie)Show the VehicleTable based on DealerID
-    JScrollPane js=new JScrollPane(table);
+    JScrollPane js = new JScrollPane(table);
     js.setBounds(80, 60, 400, 250);
     js.setBackground(Color.LIGHT_GRAY);
     table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
@@ -81,14 +84,16 @@ public class InventoryInformation extends JFrame {
     JButton btn1 = new JButton("Modify");
     btn1.setBounds(120, 320, 120, 40);
     JButton btn2 = new JButton("Delete");
-    btn2.setBounds(300, 320, 120, 40);
+    btn2.setBounds(320, 320, 120, 40);
     JButton btn3 = new JButton("Add Vehicles");
-    btn3.setBounds(120, 380, 300, 40);
-    JButton[] jButtons = new JButton[]{btn1, btn2, btn3};
+    btn3.setBounds(120, 380, 120, 40);
+    JButton btn4 = new JButton("Back");
+    btn4.setBounds(320, 380, 120, 40);
+    JButton[] jButtons = new JButton[]{btn1, btn2, btn3, btn4};
     Dimension preferredSize = new Dimension(120, 40);
     for (JButton jButton : jButtons) {
       jButton.setPreferredSize(preferredSize);
-      jButton.setBackground(Color.blue);
+      jButton.setBackground(Color.white);
       jButton.setOpaque(true);
       jButton.setFont(new Font("Arial", PLAIN, 15));
       panel.add(jButton);
@@ -98,46 +103,55 @@ public class InventoryInformation extends JFrame {
       @Override
       public void actionPerformed(ActionEvent e) {
         //Get the VehicleID selected vehicle
-        int rowIndex = (int) table.getSelectedRow();
-        int vehID=(int)table.getValueAt(rowIndex,0);
-        Collection<Vehicle> veh = vmi.getVehiclesBasedOnDealerId(dID);
-        Vehicle modifyV = new Vehicle();
-        for (Vehicle v : veh) {
-          if (v.getVehicleId() == vehID) {
-            modifyV = v;
+        try {
+          int rowIndex = table.getSelectedRow();
+          int vehID = (int) table.getValueAt(rowIndex, 0);
+          Collection<Vehicle> veh = vmi.getVehiclesBasedOnDealerId(dID);
+          Vehicle modifyV = new Vehicle();
+          for (Vehicle v : veh) {
+            if (v.getVehicleId() == vehID) {
+              modifyV = v;
+            }
           }
+          new ModifyInventory(modifyV);
+          frame.dispose();
+        } catch (Exception ex) {
+          JOptionPane.showMessageDialog(panel, "Please select one vehicle to modify!");
         }
-        new ModifyInventory(modifyV);
-        frame.dispose();
+
       }
     });
     //(Ekie)Delete vehicles
     btn2.addActionListener(e -> {
-      //Step1: Delete the vehicle from the db
-      int rowIndex = (int) table.getSelectedRow();
-      int vehID=(int)table.getValueAt(rowIndex,0);
-      Vehicle deleteV = new Vehicle();
-      Collection<Vehicle> veh = vmi.getVehiclesBasedOnDealerId(dID);
-      //Use vehicleID to get Vehicle and pass it to deleteVehicle()
-      for (Vehicle v : veh) {
-        if (v.getVehicleId() == vehID) {
-          deleteV = v;
+      try {
+        //Step1: Delete the vehicle from the db
+        int rowIndex = (int) table.getSelectedRow();
+        int vehID = (int) table.getValueAt(rowIndex, 0);
+        Vehicle deleteV = new Vehicle();
+        Collection<Vehicle> veh = vmi.getVehiclesBasedOnDealerId(dID);
+        //Use vehicleID to get Vehicle and pass it to deleteVehicle()
+        for (Vehicle v : veh) {
+          if (v.getVehicleId() == vehID) {
+            deleteV = v;
+          }
         }
-      }
-      vmi.deleteVehicle(deleteV);
-      table.updateUI();
-      //Step2: Delete the vehicle on the screen
-      JOptionPane.showMessageDialog(table, "Vehicle " + deleteV.getVehicleId() + " has been deleted");
-      VModel.removeRow(rowIndex);
-      int size = VModel.getRowCount();
-      if (size == 0) { //No vehicles left, disable delete.
-        btn2.setEnabled(false);
-      } else { //Select an index.
-        if (rowIndex == size) {
-          rowIndex--;
-        }
+        vmi.deleteVehicle(deleteV);
+        table.updateUI();
+        //Step2: Delete the vehicle on the screen
+        JOptionPane.showMessageDialog(table, "Vehicle " + deleteV.getVehicleId() + " has been deleted");
+        vModel.removeRow(rowIndex);
+        int size = vModel.getRowCount();
+        if (size == 0) { //No vehicles left, disable delete.
+          btn2.setEnabled(false);
+        } else { //Select an index.
+          if (rowIndex == size) {
+            rowIndex--;
+          }
         /*table.setSelectedIndex(index);
         table.ensure;*/
+        }
+      } catch (Exception ex) {
+        JOptionPane.showMessageDialog(panel, "Please select one vehicle to delete!");
       }
     });
 
@@ -146,6 +160,14 @@ public class InventoryInformation extends JFrame {
       public void actionPerformed(ActionEvent e) {
         new AddVehicles(dID);
         frame.dispose();
+      }
+    });
+
+    btn4.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        frame.dispose();
+        new OperationOptions(dID);
       }
     });
   }
